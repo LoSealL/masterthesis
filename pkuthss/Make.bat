f2b5f339fde613c1879c95f4ed6906302b9b4eb7
唐文懿 <twytwy12345@live.com>
2016-03-21 11:00:59 +0800

    Initial update

---
+++ pkuthss/Make.bat
@@ -0,0 +1,39 @@
+@echo off
+
+@rem vim:ts=4:sw=4
+@rem
+@rem Copyright (c) 2008-2009 solvethis
+@rem Copyright (c) 2010-2012 Casper Ti. Vector
+@rem Public domain.
+
+set JOBNAME=thesis
+@rem 这个变量的值可以为 latex 或 pdflatex。
+set LATEX=pdflatex
+set BIBTEX=biber -l zh__pinyin
+set DVIPDF=dvipdfmx
+
+if "%LATEX%"=="latex" (set DODVIPDF=%DVIPDF% %JOBNAME%
+) else (set DODVIPDF=echo No need to run %DVIPDF%.)
+if "%1"=="" goto doc
+if "%1"=="doc" goto doc
+if "%1"=="clean" (goto clean) else (goto usage)
+
+:doc
+%LATEX% %JOBNAME%
+%BIBTEX% %JOBNAME%
+%LATEX% %JOBNAME%
+%LATEX% %JOBNAME%
+REM %DODVIPDF%
+goto end
+
+:clean
+del /Q *.aux *.bbl *.bcf *.blg *.dvi *.lof *.log *.lot *.run.xml *.toc *.out
+del /Q missfont.log chap\*.aux
+goto end
+
+:usage
+echo %0 [doc] [clean]
+goto end
+
+:end
+
